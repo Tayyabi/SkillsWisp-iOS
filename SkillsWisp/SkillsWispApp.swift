084b4a5b97324a2300d79be63450063bf42a70af
Muhammad Tayyab <muhammadtayyab703@gmail.com>
2023-05-08 18:04:11 +0400

    Initial Commit

---
+++ SkillsWisp/SkillsWispApp.swift
@@ -0,0 +1,20 @@
+//
+//  SkillsWispApp.swift
+//  SkillsWisp
+//
+//  Created by M Tayyab on 08/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SkillsWispApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
